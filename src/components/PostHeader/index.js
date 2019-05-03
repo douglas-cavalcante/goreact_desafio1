ac0be788a6f249b83ee61581c3b04b70908acbbd
douglas-cavalcante <henrique.douglas.costaa@gmail.com>
2019-05-03 00:35:34 -0300

    Adiciona prop-types em HeaderPost

--- src/components/PostHeader/index.js
+++ src/components/PostHeader/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const HeaderPost = ({ avatar, name, time }) => (
   <div className="header-post">
-    <img className="avatar" src={avatar} açt="avatar" />
+    <img className="avatar" src={avatar} alt="avatar" />
     <div className="info-post">
       <strong>{name}</strong>
       <span>{time}</span>
     </div>
   </div>
 );
+
+HeaderPost.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+};
 
 export default HeaderPost;
